1e547ec5697ff4b132c7c9b2a33fe6e251fd19b1
esti08-hu <estioame@gmail.com>
2024-08-09 16:29:48 +0300

    checked pycodestyle

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -3,7 +3,9 @@
 '''
 import asyncio
 
+
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     '''Creates an asynchronous task for wait_random.
